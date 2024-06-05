0e69ffa74f5193b03a8a63ad5e06968d43ab0c3a
oeuvars <www.anuragniall@gmail.com>
2024-06-05 15:49:51 +0530

    google site verifiaction added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { ClerkProvider } from "@clerk/nextjs";
 import ModalProvider from "@/providers/modal-provider";
 import { BillingProvider } from "@/providers/billing-provider";
 import { Toaster } from "react-hot-toast";
-import { dark, neobrutalism } from "@clerk/themes";
 
 export const metadata: Metadata = {
   title: "Mercao",
@@ -19,6 +18,9 @@ export default function RootLayout({
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
+        <head>
+          <meta name="google-site-verification" content="whgGbmwHfJwHnJJqghEexN8OGasfii20SNMbgXr_Iuk" />
+        </head>
         <body>
           <ModalProvider>
             <BillingProvider>
