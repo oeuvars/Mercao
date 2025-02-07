77d49fbf795dcfdc68bee04bc390fdc542f2c850
oeurags <oeuvars@gmail.com>
2025-02-07 17:16:23 +0530

    fix: build fix try I

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/components/global/navbar";
-import Hero from "./(home)/hero";
-import Pricing from "./(home)/pricing";
+import Hero from "./(base)/hero";
+import Pricing from "./(base)/pricing";
 import Footer from "../components/global/footer";
-import Solution from "./(home)/solution";
-import Testimonials from "./(home)/testimonials";
+import Solution from "./(base)/solution";
+import Testimonials from "./(base)/testimonials";
 
 export default function Home() {
   return (
